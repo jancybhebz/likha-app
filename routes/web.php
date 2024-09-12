3657f0e63af6143301382ba0c69f25bcf5ce7ff3
jancy <frjdelarosa@gmail.com>
2024-09-13 03:31:56 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home-landing');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
